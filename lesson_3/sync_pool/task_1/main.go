0b8dcc295b5869f61992ed66e7b7561e4da34a84
shkly <shklyarov.vlad@bk.ru>
2025-11-30 10:57:57 +0300

    task 1 sync_pool completed

--- lesson_3/sync_pool/task_1/main.go
+++ lesson_3/sync_pool/task_1/main.go
@@ -16,16 +16,16 @@ func main() {
 	pool := &syncPool.SyncPool{}
 
 	for _, s := range examples {
-		processed := ProcessString(s)
+		processed := ProcessString(pool, s)
 		fmt.Printf("Original: %q\nProcessed: %q\n\n", s, processed)
 	}
 }
 
-func ProcessString(s string) string {
+func ProcessString(pool *syncPool.SyncPool, s string) string {
 	buf := syncPool.BuffersPool.Get().(*[]byte) // берем буфер из пула
 	*buf = append(*buf, strings.ToUpper(s)...)  // добавляем в него обработанную строку
 	result := string(*buf)                      // сохраняем в локальной переменной
 
-	syncPool.BuffersPool.(buf) // возвращаем буфер обратно в пул
+	pool.PutBytes(buf) // возвращаем буфер обратно в пул
 	return result
 }
